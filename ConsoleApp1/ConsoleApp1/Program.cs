3d7fcd6322a186492c66e57bf285e8bbc6dc97b2
Kevin Willford <kewillf@microsoft.com>
2018-05-14 10:56:11 -0600

    Add any to the message for the user

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Press key to exit.");
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
